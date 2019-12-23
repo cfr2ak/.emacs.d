2ca1b7fe34834e3a8051aee91cab1ebcde617ebd
cfr2ak <cfr2ak@gmail.com>
2019-12-23 23:58:05 +0900

    :cat: Updates

---
+++ elpa/rtags-20191222.920/rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rtags" "20191222.920" "A front-end for rtags" '((emacs "24.3")) :commit "595055b5316a7c92ba1d638f324f98842a0f41a5" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "https://github.com/Andersbakken/rtags")
