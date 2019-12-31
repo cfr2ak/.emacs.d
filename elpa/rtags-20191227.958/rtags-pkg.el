d682403a94c175cbd1cbfb4750c7c0f3536fee70
cfr2ak <cfr2ak@gmail.com>
2019-12-31 11:59:25 +0900

    :wrench: auto format

---
+++ elpa/rtags-20191227.958/rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rtags" "20191227.958" "A front-end for rtags" '((emacs "24.3")) :commit "c15ee631a40d71086731afd35785a65cc5ba6dbc" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "https://github.com/Andersbakken/rtags")
